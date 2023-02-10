5d13137eabc8d54d69ec82f3e2b2616379ef15a5
danielaCurci <121577362+Daniela-9@users.noreply.github.com>
2023-02-10 16:53:40 +0100

    Add files via upload

---
+++ exercise-1.js
@@ -0,0 +1,16 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+
+Object.keys(person).forEach(p => {
+  console.log(`${p}: ${person[p]}`)
+})
+
+// Print values of person using Object.keysconst object1 = {
+
+
+
+
