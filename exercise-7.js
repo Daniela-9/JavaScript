66c7440087861a39f138d62fe31e6898db179a2b
danielaCurci <121577362+Daniela-9@users.noreply.github.com>
2023-02-17 20:21:38 +0100

    Add files via upload

---
+++ exercise-7.js
@@ -0,0 +1,51 @@
+class Person {
+  firstName = "";
+  lastName = "";
+  age = 0;
+
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  set firstName(newName){
+    this.firstName = this.newName;
+  }
+
+  get firstName(){
+    return this.firstName;
+  }
+
+  set lastName(newLast){
+    this.lastName = this.newLast;
+  }
+  get lastName(){
+    return this.lastName;
+  }
+
+  set age(newAge){
+    this.age = this.newAge;
+  }
+  get age(){
+    return this.age
+  }
+  get fullName(){
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
+
+
+
+
+
+//Define a class called `Person` that takes in three parameters in the constructor (`firstName`, `lastName` and `age`). 
+//Create the getters and setters methods for each property and a getter method called `fullName` that returns the full name.
